d9bdf096fd95f7991e9646382135e600c9a91961
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-28 20:00:10 +0200

    change font

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,400..900&family=Share+Tech+Mono&display=swap" rel="stylesheet">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Laravel trains</title>
 </head>
 <body>
 
     <section>
-        <div class="container p-4">
+        <div class="container p-4 share-tech-mono-regular playfair-display">
             <div class="row row-cols-1 g-3">
                     @foreach ($trains as $train)
                          <div class="col">
