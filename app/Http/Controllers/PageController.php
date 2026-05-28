3581745e04f525ea6d874fc10615e2373bc76214
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-28 18:43:26 +0200

    create function index , rename home view

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+
+    public function index() {
+        $trains = Train::all();
+        return view('home', compact('trains'));
+        }
+
 }
