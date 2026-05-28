9cd51217011a7c000671a36fd2d7732da12f97bb
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-28 19:43:36 +0200

    css adjustments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,27 +10,39 @@
 <body>
 
     <section>
-        <div class="container">
-            <div class="row row-cols-3 g-3">
+        <div class="container p-4">
+            <div class="row row-cols-1 g-3">
                     @foreach ($trains as $train)
                          <div class="col">
                             <div class="card h-100">
                                 <div class="d-flex justify-content-around">
-                                    <div class="d-flex flex-column justify-content-around gap-2">
+                                    <div class="d-flex flex-column align-items-center justify-content-around gap-2">
+                                         <small class="fw-semibold">Compagnia</small>
                                         <p>{{$train['company_name']}}</p>
+                                        <small class="fw-semibold">Codice</small>
                                         <p>{{$train['train_code']}}</p>
                                     </div>
-                                    <div class="d-flex flex-column justify-content-around gap-2">
-                                        <p>{{$train['departure_station']}}<p>
+                                    <div class="d-flex flex-column align-items-center justify-content-around gap-2">
+                                        <small class="fw-semibold">Da</small>
+                                        <p>{{$train['departure_station']}}</p>
+                                        <small class="fw-semibold">A</small>
                                         <p>{{$train['arrival_station']}}</p>
                                     </div>
-                                    <div class="d-flex flex-column gap-3 justify-content-around">
+                                    <div class="d-flex flex-column align-items-center gap-2 justify-content-around">
+                                        <small class="fw-semibold">Partenza</small>
                                         <p>{{$train['departure_time']}}</p>
+                                        <small class="fw-semibold">Arrivo</small>
                                         <p>{{$train['arrival_time']}}</p>
                                     </div>
-                                    <div class="d-flex flex-column gap-3 justify-content-around">
+                                    <div class="d-flex flex-column align-items-center gap-2 justify-content-around">
+                                        <small class="fw-semibold">Carrozze</small>
                                         <p>{{$train['carriage_count']}}</p>
-                                        <p>{{$train['is_delayed']}}</p>
+                                        <small class="fw-semibold">Ritardoo</small>
+                                        @if ($train['is_delayed'])
+                                            <p>SI</p>
+                                        @else
+                                            <p>NO</p>
+                                        @endif
                                     </div>
                                 </div>
                             </div>
