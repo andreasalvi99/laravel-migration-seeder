9e68e3d2342f72eded3f6a6e1b02e68b86123dcc
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-01 16:01:47 +0200

    add for loop

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -14,19 +14,21 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        $newTrain = new Train();
+        for($i=0; $i <= 20; $i++) {
+            $newTrain = new Train();
 
-        $newTrain->company_name = $faker->company;
-        $newTrain->departure_station = $faker->city;
-        $newTrain->arrival_station = $faker->city;
-        $newTrain->departure_time = $faker->time;
-        $newTrain->arrival_time = $faker->time;
-        $newTrain->train_code = $faker->bothify('????-####');
-        $newTrain->carriage_count = $faker->numberBetween(5,13);
-        $newTrain->is_delayed = $faker->boolean(0.5);
-        $newTrain->is_cancelled = $faker->boolean(0.5);
+            $newTrain->company_name = $faker->company;
+            $newTrain->departure_station = $faker->city;
+            $newTrain->arrival_station = $faker->city;
+            $newTrain->departure_time = $faker->time;
+            $newTrain->arrival_time = $faker->time;
+            $newTrain->train_code = $faker->bothify('????-####');
+            $newTrain->carriage_count = $faker->numberBetween(5,13);
+            $newTrain->is_delayed = $faker->boolean(0.5);
+            $newTrain->is_cancelled = $faker->boolean(0.5);
 
 
-        $newTrain->save();
+            $newTrain->save();
+        }
     }
 }
