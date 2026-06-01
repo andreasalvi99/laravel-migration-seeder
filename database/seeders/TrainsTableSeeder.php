be5d0b674eef8c954eb00f5e9c8357a6952ffbad
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-06-01 15:58:27 +0200

    add fakers

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,25 +5,26 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Train;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         $newTrain = new Train();
 
-        $newTrain->company_name = 'Italo';
-        $newTrain->departure_station = 'Roma Termini';
-        $newTrain->arrival_station = 'Messina';
-        $newTrain->departure_time = '08:30:00';
-        $newTrain->arrival_time = '12:30:00';
-        $newTrain->train_code = 'ITL310';
-        $newTrain->carriage_count = 4;
-        $newTrain->is_delayed = false;
-        $newTrain->is_cancelled = false;
+        $newTrain->company_name = $faker->company;
+        $newTrain->departure_station = $faker->city;
+        $newTrain->arrival_station = $faker->city;
+        $newTrain->departure_time = $faker->time;
+        $newTrain->arrival_time = $faker->time;
+        $newTrain->train_code = $faker->bothify('????-####');
+        $newTrain->carriage_count = $faker->numberBetween(5,13);
+        $newTrain->is_delayed = $faker->boolean(0.5);
+        $newTrain->is_cancelled = $faker->boolean(0.5);
 
 
         $newTrain->save();
